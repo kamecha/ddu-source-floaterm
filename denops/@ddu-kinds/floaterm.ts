bae4f38b63d86414435dd3597bfe1150bef3f8db
kamecha <masakikameyama81@gmail.com>
2023-05-25 12:12:20 +0900

    previewの追加(本体がbufexists判定する必要あり)

--- denops/@ddu-kinds/floaterm.ts
+++ denops/@ddu-kinds/floaterm.ts
@@ -1,4 +1,11 @@
-import { ActionFlags, Actions, BaseKind, DduItem } from "https://deno.land/x/ddu_vim@v2.5.0/types.ts";
+import { GetPreviewerArguments } from "https://deno.land/x/ddu_vim@v2.5.0/base/kind.ts";
+import {
+  ActionFlags,
+  Actions,
+  BaseKind,
+  DduItem,
+  Previewer,
+} from "https://deno.land/x/ddu_vim@v2.5.0/types.ts";
 import { Denops } from "https://deno.land/x/denops_core@v4.0.0/denops.ts";
 
 export interface ActionData {
@@ -16,13 +23,29 @@ export class Kind extends BaseKind<Params> {
       for (const item of args.items) {
         if (item.action) {
           const action = item.action as ActionData;
-          await args.denops.call("floaterm#terminal#open_existing", action.bufnr);
+          await args.denops.call(
+            "floaterm#terminal#open_existing",
+            action.bufnr,
+          );
         }
       }
       return ActionFlags.None;
-    }
+    },
   };
   params(): Params {
     return {};
   }
+  async getPreviewer(
+    args: GetPreviewerArguments,
+  ): Promise<Previewer | undefined> {
+    const action = args.item.action as ActionData;
+    if (!action) {
+      return undefined;
+    }
+    return {
+      kind: "buffer",
+      expr: action.bufnr,
+    }
+  }
 }
+
