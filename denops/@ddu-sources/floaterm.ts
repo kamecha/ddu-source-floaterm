2e7406658ef018ab391466b5ded131dbdc286e77
kamecha <masakikameyama81@gmail.com>
2023-05-25 13:00:51 +0900

    表示する箇所をLeaderF-floatermを参考に変更

--- denops/@ddu-sources/floaterm.ts
+++ denops/@ddu-sources/floaterm.ts
@@ -2,7 +2,11 @@ import { BaseSource, Item } from "https://deno.land/x/ddu_vim@v2.5.0/types.ts";
 import { Denops } from "https://deno.land/x/denops_core@v4.0.0/denops.ts";
 import { ActionData } from "../@ddu-kinds/floaterm.ts";
 import { ensureArray } from "https://deno.land/x/unknownutil@v2.1.0/ensure.ts";
-import { isNumber } from "https://deno.land/x/unknownutil@v2.1.0/mod.ts";
+import {
+  isNumber,
+  isObject,
+} from "https://deno.land/x/unknownutil@v2.1.0/mod.ts";
+import { fn } from "https://deno.land/x/ddu_vim@v2.5.0/deps.ts";
 
 type Param = Record<never, never>;
 
@@ -19,8 +23,14 @@ export class Source extends BaseSource<Param> {
         );
         const items: Item<ActionData>[] = [];
         for (const bufnr of floatermBufList) {
+          const bufInfo = ensureArray(
+            await fn.getbufinfo(args.denops, bufnr),
+            isObject,
+          );
+          const bufname = bufInfo[0].bufname as string;
+          const title = await fn.getbufvar(args.denops, bufnr, "term_title") as string;
           items.push({
-            word: `floaterm#${bufnr}`,
+            word: `${bufnr} ${bufname} ${title}`,
             action: {
               bufnr: bufnr,
             },
